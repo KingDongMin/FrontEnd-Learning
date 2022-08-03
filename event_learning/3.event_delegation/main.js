f33f5889f2cd8de876b8cac198383feb615ac8f7
RingDingDongMin <92971320+KingDongMin@users.noreply.github.com>
2022-08-03 17:58:50 +0900

    Event-shopping_refactoring
    1. 학습 내용
    - 이벤트 위임을 이용하여 쇼핑 리스트 앱 리팩토링
    
    2. 핵심 키워드
    - 자식 요소들이 공통된 이벤트 핸들링을 가질 때 이벤트 위임을 사용할 수 있다.
    - 이벤트 위임을 사용하면 각 자식 요소마다 매번 이벤트 핸들링을 해주지 않아도 된다.
    - 추가되는 자식 요소들을 구분하기 위해 고유 번호를 생성한다. (UUID?)
    - event.target과 같은 event에 관련된 값이나 querySelector를 확인 할 때 JS에서 console.log를 이용하는 것이 아닌 개발자 툴을 이용하여 확인하는 것이 능률적이다.

---
+++ event_learning/3.event_delegation/main.js
@@ -0,0 +1,90 @@
+const items = document.querySelector('.items');
+const input = document.querySelector('.footer__input');
+const addBtn = document.querySelector('.footer__button');
+
+// 보통 함수를 정의할때 이벤트를 정의하는 함수명은 on을 붙인다
+function onAdd(){
+    // 1. 사용자가 입력한 텍스트를 받아옴
+    const text = input.value;
+    if(text === ''){
+        input.focus();
+        return;
+    }
+    // 2. 새로운 아이템을 만듬 (텍스트 + 삭제버튼)
+    const item = creatItem(text);
+    // 3. items 컨테이너안에 새로 만든 아이템을 추가한다.
+    items.appendChild(item);
+    // 4. 인풋을 초기화한다.
+    input.value ='';
+    // 포커스는 다시 커서가 생기는 것을 말한다.
+    input.focus();
+}
+
+let id = 0;
+function creatItem(text){
+    const itemRow = document.createElement('li');
+    itemRow.setAttribute('class','item__row');
+    itemRow.setAttribute('data-id', id);
+
+    // const item = document.createElement('div');
+    // item.setAttribute('class', 'item');
+
+    // const name = document.createElement('span');
+    // name.setAttribute('class','item__name');
+    // name.innerText = text;
+
+    // const deleteBtn = document.createElement('button');
+    // deleteBtn.setAttribute('class','item__delete');
+    // deleteBtn.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
+
+    // const itemDivider = document.createElement('div');
+    // itemDivider.setAttribute('class', 'item__divider');
+
+    // itemRow.appendChild(item);
+    // itemRow.appendChild(itemDivider);
+
+    // item.appendChild(name);
+    // item.appendChild(deleteBtn);
+    itemRow.innerHTML = `
+    <div class="item">
+        <span class="item__name">${text}</span>
+        <button class="item__delete" >
+            <i class="fa-solid fa-trash-can" data-id =${id}></i>
+        </button>
+    </div>
+    <div class="item__divider"></div>
+    `;
+    id++;
+
+    return itemRow;
+}
+
+// {내가 만든 이벤트 위임}
+// items.addEventListener('click', event=>{
+//     console.log(event.target.tagName);
+//     if(event.target.tagName == 'I'){
+//         console.log(event.target.parentNode.parentNode.parentNode);
+//         items.removeChild(event.target.parentNode.parentNode.parentNode);
+//     }
+// })
+
+items.addEventListener('click', event=>{
+    console.log(event.target.dataset.id);
+    const id = event.target.dataset.id;
+    if(id){
+        const item = document.querySelector(`.item__row[data-id='${id}']`);
+        item.remove();
+    }
+    
+})
+
+
+input.addEventListener('keypress', (event)=>{
+    if(event.key === 'Enter'){
+        onAdd();
+    }
+})
+
+addBtn.addEventListener('click', ()=>{
+    onAdd();
+})
